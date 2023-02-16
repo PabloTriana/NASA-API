41a4363b32abdd39152c8d586a4c16a45a32ca01
MintLapTop <awedeus@gmail.com>
2023-02-16 14:35:07 -0800

    Image shows in the webpage.

--- script.js
+++ script.js
@@ -6,17 +6,15 @@ searchButton.addEventListener("click", ()=>{
   sendApiRequest();
 })
 
-
 //An asynchronous function to fetch data from the API.
 async function sendApiRequest(){
   let API_KEY = "DjGaS2S70ZBCvZzdnDZbt5QSfo0802khQkiw22jf";
   let response = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=fhaz&api_key=${API_KEY}`); 
   console.log(response);
   let data = await response.json();
-  console.log(data);
-  useApiData(data);
+  useApiData(data.photos[0]);
 }
 
 function useApiData(data){
-  document.querySelector("#content").innerHTML += `<img_src = "${data.photos[0].img_src}">`;
+  document.querySelector("#content").innerHTML += `<img src = ${data.img_src}></img>`;
 }
